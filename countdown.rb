c591fc4d296290f8f6e404ff163296036aaa1daa
Dylan Crawshaw <dcrawsh@gmail.com>
2019-08-16 01:33:23 +0000

    Done.

--- countdown.rb
+++ countdown.rb
@@ -1,6 +1,5 @@
 
 def countdown(number)
-  number = 10
   while number > 0
   puts "#{number} SECOND(S)!"
   number -= 1
@@ -12,7 +11,6 @@ countdown(10)
 
 
 def countdown_with_sleep(number)
-  number = 10
   while number > 0
   puts "#{number} SECOND(S)!"
   number -= 1
