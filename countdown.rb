e00ec28535654562967ab2552df55947312a876f
Steven Laff <steve.laff@gmail.com>
2019-08-17 03:18:05 +0000

    Done.

--- countdown.rb
+++ countdown.rb
@@ -1,4 +1,18 @@
 #write your code here
 
-def countdown
+def countdown (t_minus)
+  until t_minus == 0 do
+    puts "#{t_minus} SECOND(S)!"
+    t_minus -= 1
+  end
+  return "HAPPY NEW YEAR!"
+end
+
+def countdown_with_sleep(t_minus)
+  until t_minus == 0 do
+    puts "#{t_minus} SECOND(S)!"
+    t_minus -= 1
+    sleep 1
+  end
+  return "HAPPY NEW YEAR!"
 end
