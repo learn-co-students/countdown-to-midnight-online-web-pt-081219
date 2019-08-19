7de214ec332634fe45c3b1dd25a2a207babb9f25
Katherine Walendzik <katherinewalendzik@gmail.com>
2019-08-19 15:14:28 +0000

    Done.

--- countdown.rb
+++ countdown.rb
@@ -1,4 +1,17 @@
-#write your code here
+def countdown(seconds)
+  seconds = 10
+  while seconds > 0
+  puts "#{seconds} SECOND(S)!"
+  seconds -= 1
+end
+return "HAPPY NEW YEAR!"
+end
 
-def countdown
+def countdown_with_sleep(second)
+  second = 10
+  while second > 0
+  puts "#{second} SECOND(S)!"
+  second -= 1
+  sleep 1
+end
 end
